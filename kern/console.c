2cb537b36f80b5263671bb92351078df56cd47dc
JarredAllen <jarredallen73@gmail.com>
2020-08-29 16:31:20 -0700

    Fix a few more locations that I missed

--- kern/console.c
+++ kern/console.c
@@ -30,20 +30,20 @@ delay(void)
 #define COM_DLL		0	// Out: Divisor Latch Low (DLAB=1)
 #define COM_DLM		1	// Out: Divisor Latch High (DLAB=1)
 #define COM_IER		1	// Out: Interrupt Enable Register
-#define   COM_IER_RDI	0x01	//      Enable receiver data interrupt
+#define   COM_IER_RDI	0x01	//   Enable receiver data interrupt
 #define COM_IIR		2	// In:	Interrupt ID Register
 #define COM_FCR		2	// Out: FIFO Control Register
 #define COM_LCR		3	// Out: Line Control Register
-#define	  COM_LCR_DLAB	0x80	//       Divisor latch access bit
-#define	  COM_LCR_WLEN8	0x03	//       Wordlength: 8 bits
+#define	  COM_LCR_DLAB	0x80	//   Divisor latch access bit
+#define	  COM_LCR_WLEN8	0x03	//   Wordlength: 8 bits
 #define COM_MCR		4	// Out: Modem Control Register
 #define	  COM_MCR_RTS	0x02	// RTS complement
 #define	  COM_MCR_DTR	0x01	// DTR complement
 #define	  COM_MCR_OUT2	0x08	// Out2 complement
 #define COM_LSR		5	// In:	Line Status Register
-#define   COM_LSR_DATA	0x01	//      Data available
-#define   COM_LSR_TXRDY	0x20	//      Transmit buffer avail
-#define   COM_LSR_TSRE	0x40	//      Transmitter off
+#define   COM_LSR_DATA	0x01	//   Data available
+#define   COM_LSR_TXRDY	0x20	//   Transmit buffer avail
+#define   COM_LSR_TSRE	0x40	//   Transmitter off
 
 static bool serial_exists;
 
@@ -308,52 +308,52 @@ static uint8_t togglecode[256] =
 
 static uint8_t normalmap[256] =
 {
-	NO,   0x1B, '1',  '2',  '3',  '4',  '5',  '6',  // 0x00
+	NO,   0x1B, '1',  '2',  '3',  '4',  '5',  '6',	// 0x00
 	'7',  '8',  '9',  '0',  '-',  '=',  '\b', '\t',
-	'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',  // 0x10
+	'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',	// 0x10
 	'o',  'p',  '[',  ']',  '\n', NO,  'a',  's',
-	'd',  'f',  'g',  'h',  'j',  'k',  'l',  ';',  // 0x20
+	'd',  'f',  'g',  'h',  'j',  'k',  'l',  ';',	// 0x20
 	'\'', '`',  NO,   '\\', 'z',  'x',  'c',  'v',
-	'b',  'n',  'm',  ',',  '.',  '/',  NO,   '*',  // 0x30
+	'b',  'n',  'm',  ',',  '.',  '/',  NO,   '*',	// 0x30
 	NO,   ' ',  NO,   NO,   NO,   NO,   NO,   NO,
-	NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',  // 0x40
+	NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',	// 0x40
 	'8',  '9',  '-',  '4',  '5',  '6',  '+',  '1',
-	'2',  '3',  '0',  '.',  NO,   NO,   NO,   NO,  // 0x50
-	[0xC7] = KEY_HOME,            [0x9C] = '\n' /*KP_Enter*/,
+	'2',  '3',  '0',  '.',  NO,   NO,   NO,   NO,	// 0x50
+	[0xC7] = KEY_HOME,	      [0x9C] = '\n' /*KP_Enter*/,
 	[0xB5] = '/' /*KP_Div*/,      [0xC8] = KEY_UP,
-	[0xC9] = KEY_PGUP,            [0xCB] = KEY_LF,
-	[0xCD] = KEY_RT,              [0xCF] = KEY_END,
-	[0xD0] = KEY_DN,              [0xD1] = KEY_PGDN,
-	[0xD2] = KEY_INS,             [0xD3] = KEY_DEL
+	[0xC9] = KEY_PGUP,	      [0xCB] = KEY_LF,
+	[0xCD] = KEY_RT,	      [0xCF] = KEY_END,
+	[0xD0] = KEY_DN,	      [0xD1] = KEY_PGDN,
+	[0xD2] = KEY_INS,	      [0xD3] = KEY_DEL
 };
 
 static uint8_t shiftmap[256] =
 {
-	NO,   033,  '!',  '@',  '#',  '$',  '%',  '^',  // 0x00
+	NO,   033,  '!',  '@',  '#',  '$',  '%',  '^',	// 0x00
 	'&',  '*',  '(',  ')',  '_',  '+',  '\b', '\t',
-	'Q',  'W',  'E',  'R',  'T',  'Y',  'U',  'I',  // 0x10
+	'Q',  'W',  'E',  'R',  'T',  'Y',  'U',  'I',	// 0x10
 	'O',  'P',  '{',  '}',  '\n', NO,   'A',  'S',
-	'D',  'F',  'G',  'H',  'J',  'K',  'L',  ':',  // 0x20
+	'D',  'F',  'G',  'H',  'J',  'K',  'L',  ':',	// 0x20
 	'"',  '~',  NO,   '|',  'Z',  'X',  'C',  'V',
-	'B',  'N',  'M',  '<',  '>',  '?',  NO,   '*',  // 0x30
+	'B',  'N',  'M',  '<',  '>',  '?',  NO,   '*',	// 0x30
 	NO,   ' ',  NO,   NO,   NO,   NO,   NO,   NO,
-	NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',  // 0x40
+	NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',	// 0x40
 	'8',  '9',  '-',  '4',  '5',  '6',  '+',  '1',
-	'2',  '3',  '0',  '.',  NO,   NO,  NO,   NO,  // 0x50
-	[0xC7] = KEY_HOME,            [0x9C] = '\n' /*KP_Enter*/,
+	'2',  '3',  '0',  '.',  NO,   NO,  NO,   NO,	// 0x50
+	[0xC7] = KEY_HOME,	      [0x9C] = '\n' /*KP_Enter*/,
 	[0xB5] = '/' /*KP_Div*/,      [0xC8] = KEY_UP,
-	[0xC9] = KEY_PGUP,            [0xCB] = KEY_LF,
-	[0xCD] = KEY_RT,              [0xCF] = KEY_END,
-	[0xD0] = KEY_DN,              [0xD1] = KEY_PGDN,
-	[0xD2] = KEY_INS,             [0xD3] = KEY_DEL
+	[0xC9] = KEY_PGUP,	      [0xCB] = KEY_LF,
+	[0xCD] = KEY_RT,	      [0xCF] = KEY_END,
+	[0xD0] = KEY_DN,	      [0xD1] = KEY_PGDN,
+	[0xD2] = KEY_INS,	      [0xD3] = KEY_DEL
 };
 
 #define C(x) (x - '@')
 
 static uint8_t ctlmap[256] =
 {
-	NO,      NO,	  NO,	   NO,       NO,     NO,      NO,      NO,
-	NO,      NO,	  NO,	   NO,       NO,     NO,      NO,      NO,
+	NO,      NO,      NO,      NO,      NO,      NO,      NO,      NO,
+	NO,      NO,      NO,      NO,      NO,      NO,      NO,      NO,
 	C('Q'),  C('W'),  C('E'),  C('R'),  C('T'),  C('Y'),  C('U'),  C('I'),
 	C('O'),  C('P'),  NO,      NO,      '\r',    NO,      C('A'),  C('S'),
 	C('D'),  C('F'),  C('G'),  C('H'),  C('J'),  C('K'),  C('L'),  NO,
