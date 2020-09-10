e68d1026e0bb00ddac612e886824e5b2d28707bf
JarredAllen <jarredallen73@gmail.com>
2020-09-09 20:35:47 -0700

    Add showmappings to command list

--- kern/monitor.c
+++ kern/monitor.c
@@ -26,6 +26,7 @@ static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
 	{ "backtrace", "Display backtrace", mon_backtrace },
+        { "showmappings", "Display physical pages mapped to virtual addresses", mon_showmappings },
 };
 
 /***** Implementations of basic kernel monitor commands *****/
