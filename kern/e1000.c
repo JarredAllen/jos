bf4130b150207dec692b3168e6788b7510d8f8ed
ievans-HMC <ievans@g.hmc.edu>
2020-11-04 19:58:51 -0800

    Completed Exercises 2-4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,4 +1,18 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
+#include <kern/pci.h>
 
 // LAB 6: Your driver code here
+
+void * e1000_BAR0;
+
+#define E_REG(offset) (*(int *) (e1000_BAR0 + (offset)))
+
+int
+attach_e1000(struct pci_func * pcif)
+{
+	pci_func_enable(pcif);
+	e1000_BAR0 = mmio_map_region(pcif->reg_base[0],pcif->reg_size[0]);
+	cprintf("e1000 status: 0x%x\n", E_REG(E1000_STATUS));
+	return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,3 +1,14 @@
 #ifndef JOS_KERN_E1000_H
+
+#include <kern/pci.h>
+
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+
+#define E1000_VND_ID	0x8086	/* Vendor ID */
+#define E1000_DEV_ID	0x100E	/* Device ID */
+
+
+int attach_e1000(struct pci_func * pcif);
+
 #define JOS_KERN_E1000_H
 #endif  // SOL >= 6
