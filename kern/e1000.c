f54bb825d598b007d2b49090716703ba3b383b6f
ievans-HMC <ievans@g.hmc.edu>
2020-11-11 18:06:19 -0800

    completed Exercises 5-6

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,10 +1,12 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 #include <kern/pci.h>
+#include <inc/error.h>
 
 // LAB 6: Your driver code here
 
 void * e1000_BAR0;
+struct tx_desc * transmit_buf;
 
 #define E_REG(offset) (*(uint32_t *) (e1000_BAR0 + (offset)))
 
@@ -14,5 +16,37 @@ attach_e1000(struct pci_func * pcif)
 	pci_func_enable(pcif);
 	e1000_BAR0 = mmio_map_region(pcif->reg_base[0],pcif->reg_size[0]);
 	cprintf("e1000 status: 0x%x\n", E_REG(E1000_STATUS));
+	struct PageInfo * buf_pg = page_alloc(ALLOC_ZERO);
+	transmit_buf = page2kva(buf_pg);
+	E_REG(E1000_TDBAL) = page2pa(buf_pg);
+	E_REG(E1000_TDLEN) = E1000_TBUFSIZE;
+	E_REG(E1000_TDH)   = 0;
+	E_REG(E1000_TDT)   = 0;
+	E_REG(E1000_TCTL)  = E1000_TCTL_EN 
+			   | E1000_TCTL_PSP 
+			   | (E1000_TCTL_CT & 0x100) 
+			   | (E1000_TCTL_COLD & 0x40000);
+	E_REG(E1000_TIPG)  = 10 | (8 << 10) | (6 << 20); 
+	for (int i = 0; i < E1000_TBUFCNT; i++) {
+		transmit_buf[i].status = E1000_TXSTAT_DD;
+	}
+	send_data(0, 0, 1);
+	return 0;
+}
+
+int
+send_data(void * start, int len, int eop)
+{
+	#define tail E_REG(E1000_TDT)
+	if (!(transmit_buf[tail].status & E1000_TXSTAT_DD)) {
+		return -E_NO_MEM;
+	}
+	transmit_buf[tail] = (struct tx_desc) {
+		.addr   = (uint32_t) start, 
+		.length = (uint16_t) len, 
+		.cmd = E1000_TXCMD_RS | eop, 
+	};
+	tail = (tail + 1) % E1000_TBUFCNT;
+	#undef tail
 	return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -2,13 +2,44 @@
 
 #include <kern/pci.h>
 
-#define E1000_STATUS   0x00008  /* Device Status - RO */
-
 #define E1000_VND_ID	0x8086	/* Vendor ID */
 #define E1000_DEV_ID	0x100E	/* Device ID */
 
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descriptor Tail - RW */
+
+#define E1000_TBUFSIZE 1024	/* Transmit Buffer Size */
+#define E1000_TBUFCNT  (E1000_TBUFSIZE/sizeof(struct tx_desc)) /* Size of Buffer Array */
+
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+
+#define E1000_TXCMD_RS  8
+#define E1000_TXSTAT_DD 1
+
+struct tx_desc
+{
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+};
 
 int attach_e1000(struct pci_func * pcif);
+int send_data(void * start, int len, int eop);
 
 #define JOS_KERN_E1000_H
 #endif  // SOL >= 6
